78f4368fe18ff71a09969b4564ea05bcf7d0d696
Stewart <stewart.kerns@gmail.com>
2022-07-25 21:54:27 -0700

    Initial commit for Hello World of lab0

---
+++ src/kerns_lab0/Hello.java
@@ -0,0 +1,16 @@
+package kerns_lab0;
+/**This program prints Hello World! to the terminal
+ *
+ * @author Stewart Kerns
+ * @version 1.0
+ */
+public class Hello {
+    /**The main method prints Hello World! to the terminal
+     *
+     * @param args a string array containing the command line arguments
+     */
+    public static void main(String[] args){
+        //Print Hello World!
+        System.out.println("Hello World!");
+    }
+}
